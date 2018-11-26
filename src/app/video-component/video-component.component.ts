3dc3bcfe2d432090cfc5570c75c45fcce2605b36
Chaitanya Naik <chaitanya.naik@zeuslearning.com>
2018-11-26 18:29:59 +0530

    save blob, upload service (experimental)

--- src/app/video-component/video-component.component.ts
+++ src/app/video-component/video-component.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
- 
+import * as Vtt from 'vtt-creator';
+import 'rxjs/Rx';
+
 // const URL = '/api/';
-const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
+const URL = 'http://localhost:4200/upload';
 
 @Component({
   selector: 'app-video-component',
@@ -11,20 +13,41 @@ const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 })
 export class VideoComponentComponent implements OnInit {
 
-  public uploader:FileUploader = new FileUploader({url: URL});
-  public hasBaseDropZoneOver:boolean = false;
-  public hasAnotherDropZoneOver:boolean = false;
+  public uploader: FileUploader = new FileUploader({ url: URL });
+  public hasBaseDropZoneOver: boolean = false;
+  public hasAnotherDropZoneOver: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    var v = new Vtt();
+    v.add(1.05, 4, 'Never drink liquid nitrogen.', 'align:middle line:84%');
+    v.add(5, 9, ['It will perforate your stomach.', 'You could die.']);
+
+    this.saveFile(v.toString());
   }
- 
-  public fileOverBase(e:any):void {
+
+  saveFile(data) {
+    let name = 'sample.vtt';
+    let type = 'data:attachment/text';
+    if (data != null && navigator.msSaveBlob)
+      return navigator.msSaveBlob(new Blob([data], { type: type }), name);
+
+    let a = document.createElement("a");
+    document.body.appendChild(a);
+    var url = window.URL.createObjectURL(new Blob([data], { type: type }));
+    a.href = url;
+    a.download = name;
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
+  }
+  
+  public fileOverBase(e: any): void {
     this.hasBaseDropZoneOver = e;
   }
- 
-  public fileOverAnother(e:any):void {
+
+  public fileOverAnother(e: any): void {
     this.hasAnotherDropZoneOver = e;
   }
 
