b9e0ef549a4be1939c676896f859e7dff8877b9b
Chaitanya Naik <chaitanya.naik@zeuslearning.com>
2018-12-04 18:52:47 +0530

    move editor logic to main page

--- src/app/subtitle-editor/subtitle-editor.component.ts
+++ src/app/subtitle-editor/subtitle-editor.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SubtitleEditorComponent implements OnInit {
 
-  @Input() subtitleJson = {};
+  @Input() subtitleJson = [];
 
   constructor() { }
 
